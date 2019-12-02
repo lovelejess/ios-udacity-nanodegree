bf25225f2525a8c3209a3e3cf542b66548940c93
Jessica Le <c-jessica.le@charter.com>
2019-12-01 21:19:24 -0700

    Use AVFoundation to playback audio

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -7,21 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
     
     @IBOutlet weak var slowButton: UIButton!
     @IBOutlet weak var fastButton: UIButton!
     @IBOutlet weak var lowPitchButton: UIButton!
     @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("RECORDED AUDIO URL: \(recordedAudioURL)")
-
+        print(recordedAudioURL.absoluteString)
+        setupAudio()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        configureUI(.notPlaying)
+    }
+    
+    // MARK: Actions
+
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        switch(ButtonType(rawValue: sender.tag)!) {
+            case .slow:
+                playSound(rate: 0.5)
+            case .fast:
+                playSound(rate: 1.5)
+            case .chipmunk:
+                playSound(pitch: 1000)
+            case .vader:
+                playSound(pitch: -1000)
+            case .echo:
+                playSound(echo: true)
+            case .reverb:
+                playSound(reverb: true)
+            }
+
+        configureUI(.playing)
+    }
+
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
     }
     
 
