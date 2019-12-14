2bc2cd941efaeb9342505323da17f57cdd564deb
Jessica Le <c-jessica.le@charter.com>
2019-12-14 12:36:11 -0700

    Add experiment repo

---
+++ Experiment/Experiment/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Experiment
+//
+//  Created by Jess Le on 12/14/19.
+//  Copyright © 2019 lovelejess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var button: UIButton?
+    lazy var presentButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Present", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(experiment), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.addSubview(presentButton)
+        view.backgroundColor = .purple
+
+        NSLayoutConstraint.activate([
+            presentButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+                
+    }
+
+    @objc func experiment() {
+        let imagePickerController = UIImagePickerController()
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+
+
+}
+
---
+++ Experiment/README.md
@@ -0,0 +1,36 @@
+# Experiment - iOS Udacity App
+
+View Controllers and Segues Lesson
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+
+### Prerequisites
+
+#### Versions
+
+* **Swift Version**: 5.0
+* **XCode Version**: 11.1
+* **iOS Version**: 13
+
+### Installing
+
+1. Open Experiment.xcodeproj
+1. Choose a simulator that is supported for the versions listed above and run the app!
+
+### Demo
+
+![experiment demo](experiment-demo.gif)
+
+## Authors
+
+* **Udacity** - *Initial work* - [Udacity Nanodegree](https://www.udacity.com/course/ios-developer-nanodegree--nd003)
+* **Jess Le** - *Completed Work* -[github](https://github.com/lovelejess)
+
+
+## License
+
+This project is licensed under the MIT License
+
