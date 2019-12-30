fb12b11202c9e01f12c64bddd94781d40bdee21c
Jessica Le <c-jessica.le@charter.com>
2019-12-30 13:38:42 -0600

    Adds top label and image view

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoAlbumButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var memeImage: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+
     lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -17,72 +22,24 @@ class ViewController: UIViewController {
         return imageView
     }()
 
-    lazy var toolBar: UIToolbar = {
-        let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 44))
-        toolBar.translatesAutoresizingMaskIntoConstraints = false
-        return toolBar
-    }()
-
-    lazy var libraryButton: UIBarButtonItem = {
-        let button = UIBarButtonItem(barButtonSystemItem: .organize, target: self, action: #selector(selectFromLibrary))
-        button.width = 44
-        return button
-    }()
-    
-    lazy var cameraButton: UIBarButtonItem = {
-        let button = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.camera, target: self, action: #selector(selectFromCamera))
-        button.width = 44
-        return button
-    }()
-    
-    lazy var topLabel: UILabel = {
-        let label = UILabel()
-        label.text = "TOP"
-        label.textColor = .cyan
-        return label
-    }()
-    
-    lazy var bottomLabel: UILabel = {
-        let label = UILabel()
-        label.text = "BOTTOM"
-        label.textColor = .cyan
-        return label
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(imageView)
-        view.addSubview(toolBar)
-        view.addSubview(topLabel)
-        toolBar.setItems([libraryButton, cameraButton], animated: true)
+        topTextField.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-
-        NSLayoutConstraint.activate([
-            imageView.heightAnchor.constraint(equalToConstant: 500),
-            imageView.widthAnchor.constraint(equalToConstant: 500),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            topLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            topLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            topLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            toolBar.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
-            toolBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            toolBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            toolBar.heightAnchor.constraint(equalToConstant: 44)
-        ])
+        topTextField.attributedText = applyMemeStyles(textField: topTextField)
     }
 
-    @objc func selectFromLibrary() {
+    @IBAction func selectFromLibrary() {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
     }
 
-    @objc func selectFromCamera() {
+    @IBAction func selectFromCamera() {
         let imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
@@ -93,13 +50,36 @@ class ViewController: UIViewController {
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
-            imageView.image = image
-            imageView.contentMode = .scaleToFill
+            memeImage.image = image
+            memeImage.contentMode = .scaleToFill
         }
         dismiss(animated: true, completion: nil)
     }
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController {
+    func applyMemeStyles(textField: UITextField) -> NSAttributedString {
+        textField.borderStyle = .none
+        let paragraphStyle: NSMutableParagraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.center
+        let strokeTextAttributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.strokeColor : UIColor.black,
+            NSAttributedString.Key.foregroundColor : UIColor.lightGray,
+            NSAttributedString.Key.strokeWidth : -4.0,
+            NSAttributedString.Key.font : UIFont(name: "Helvetica", size: 52)!,
+            NSAttributedString.Key.paragraphStyle : paragraphStyle
+        ]
+        return NSAttributedString(string: "TOP", attributes: strokeTextAttributes)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
