adabc346590c9a34d58e6e2e3880dd4514b2d3c8
Jessica Le <c-jessica.le@charter.com>
2020-01-21 20:09:07 -0700

    Add Details View

--- MemeMe/MemeMe/MemeMeTableViewController.swift
+++ MemeMe/MemeMe/MemeMeTableViewController.swift
@@ -42,9 +42,7 @@ class MemeMeTableViewController: UITableViewController {
         let meme = memes[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "MemeMeTableViewCell", for: indexPath) as! MemeMeTableViewCell
         cell.memeImage?.image = meme.memedImage
-        cell.topText?.text = meme.topText
-        cell.bottomText?.text = meme.bottomText
-
+        cell.topText?.text = meme.topText + " " + meme.bottomText
         return cell
     }
 
