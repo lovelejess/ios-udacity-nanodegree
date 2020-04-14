a2c2cd111ad9d3a40ca55b31c597b164d5dff46c
Jessica Le <c-jessica.le@charter.com>
2020-04-13 21:42:46 -0600

    Mooskine CoreData- implement add and remove persistence

--- Mooskine-coredata/Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine-coredata/Mooskine/View Controllers/NotebooksListViewController.swift
@@ -85,15 +85,20 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource {
 
     /// Adds a new notebook to the end of the `notebooks` array
     func addNotebook(name: String) {
-//        // TODO:
-//        let notebook = Notebook(name: name)
-//        notebooks.append(notebook)
-        tableView.insertRows(at: [IndexPath(row: numberOfNotebooks - 1, section: 0)], with: .fade)
+        let notebook =  Notebook(context: dataController.viewContext)
+        notebook.name = name
+        notebook.creationDate = Date()
+        try? dataController.viewContext.save()
+        notebooks.insert(notebook, at: 0)
+        tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .fade)
         updateEditButtonState()
     }
 
     /// Deletes the notebook at the specified index path
     func deleteNotebook(at indexPath: IndexPath) {
+        let notebookToDelete = notebook(at: indexPath)
+        dataController.viewContext.delete(notebookToDelete)
+        try? dataController.viewContext.save()
         notebooks.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .fade)
         if numberOfNotebooks == 0 {
@@ -143,7 +148,7 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource {
 
     // Helper
 
-    var numberOfNotebooks: Int { return notes.count }
+    var numberOfNotebooks: Int { return notebooks.count }
 
     func notebook(at indexPath: IndexPath) -> Notebook {
         return notebooks[indexPath.row]
