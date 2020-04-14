5427a8981f5c15f31fda2a632b10d8e8fe7a5447
Jessica Le <c-jessica.le@charter.com>
2020-04-13 21:21:16 -0600

    Add Mooskine core data project

---
+++ Mooskine-coredata/Mooskine/Model/DataController.swift
@@ -0,0 +1,32 @@
+//
+//  DataController.swift
+//  Mooskine
+//
+//  Created by Jess Le on 4/13/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataController {
+    let persistentContainer: NSPersistentContainer
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(modelName: String) {
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func load(completion: (() -> Void)? = nil) {
+        persistentContainer.loadPersistentStores { (storeDescription, error) in
+            guard error == nil else {
+                fatalError(error!.localizedDescription)
+            }
+            completion?()
+        }
+    }
+}
+
