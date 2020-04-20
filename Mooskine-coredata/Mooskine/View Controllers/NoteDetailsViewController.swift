a7ceb4c46dfee10d061e2c000ae4a63ac217ec64
Jessica Le <c-jessica.le@charter.com>
2020-04-19 19:35:32 -0600

    Update Mooskine to AttributedText v2

--- Mooskine-coredata/Mooskine/View Controllers/NoteDetailsViewController.swift
+++ Mooskine-coredata/Mooskine/View Controllers/NoteDetailsViewController.swift
@@ -33,7 +33,7 @@ class NoteDetailsViewController: UIViewController {
 
         if let creationDate = note.creationDate {
             navigationItem.title = dateFormatter.string(from: creationDate)
-            textView.text = note.text
+            textView.attributedText = note.attributedText
         }
         
     }
@@ -64,7 +64,7 @@ extension NoteDetailsViewController {
 
 extension NoteDetailsViewController: UITextViewDelegate {
     func textViewDidEndEditing(_ textView: UITextView) {
-        note.text = textView.text
+        note.attributedText = textView.attributedText
         dataController.saveContext()
     }
 }
